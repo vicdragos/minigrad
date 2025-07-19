a94e173d293f4dd584fd3495a647c23f53714ace
victo <victor.dragos@icloud.com>
2025-07-19 15:52:52 +0300

    solved a bug in the _backward methods of each operation where _backward was fixed on self instead of out added the backward method

--- engine.py
+++ engine.py
@@ -12,26 +12,25 @@ class Value:
     def __repr__(self):
         return f"data = {self.data}"
 
+    # Operation definitions
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other))
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
-        self._backward = _backward
+        out._backward = _backward
         return out
 
     def __neg__(self):
         out = Value(-self.data, (self,))
         def _backward():
             self.grad += -out.grad
-        self._backward = _backward
+        out._backward = _backward
         return out
 
     def __sub__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data - other.data, (self, other))
-        return out
+        return self + (-other)
 
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
@@ -39,7 +38,7 @@ class Value:
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
-        self._backward = _backward
+        out._backward = _backward
         return out
 
     def __pow__(self, other):
@@ -48,7 +47,7 @@ class Value:
         def _backward():
             self.grad += other.data * self.data ** (other.data - 1) * out.grad
             other.grad += self.data ** other.data * log(other.data) * out.grad
-        self._backward = _backward
+        out._backward = _backward
         return out
 
     def __truediv__(self, other):
@@ -58,16 +57,31 @@ class Value:
         out = Value(exp(self.data), (self,))
         def _backward():
             self.grad += out * out.grad
-        self._backward = _backward
+        out._backward = _backward
         return out
 
     def log(self):
         out = Value(log(self.data), (self,))
         def _backward():
             self.grad += (self.data ** -1) * out.grad
-        self._backward = _backward
+        out._backward = _backward
         return out
 
+    def __rmul__(self, other):
+        return self * other
 
+    # backward method
+    def backward(self):
+        nodes = []  # Will contain each node in topological order
+        visited = set()
+        def build(v):
+            nodes.append(v)
+            if v not in visited:
+                visited.add(v)
+                for u in v._prev: build(u)
+        build(self)
+        for v in nodes:
+            v._grad = 0.0
 
-    # TODO backward() method
+        for v in nodes:
+            v._backward()
