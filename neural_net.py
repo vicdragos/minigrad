9a39dbcb80d011db1050d778937cbc4883c5079e
victo <victor.dragos@icloud.com>
2025-07-20 12:42:52 +0300

    added the Layer and MLP classes

--- neural_net.py
+++ neural_net.py
@@ -11,4 +11,24 @@ class Neuron:
         # = sum( w * x) + b
         return sum( (wt * x for wt, x in zip(self.w, x)), self.b)
 
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+
+    def __call__(self, x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1  else out
+
+class MLP:
+    def __init__(self, nin, nouts): # nouts is a list containing the no. of outputs for each layer
+        sizes = [nin] + nouts
+        self.layers = [ Layer(sizes[i], sizes[i+1]) for i in range(len(sizes))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x) # the input of the next layer is the ouput of the current layer
+        return x
+
+
+
 
