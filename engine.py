03f7d7d3ebeb4dd668088ea13ebe7271896bdaaa
victo <victor.dragos@icloud.com>
2025-07-21 02:07:02 +0300

    Added a non-linear activation function for the Neuron (tanh) Added parameters() methods to all elements of the neural net

--- engine.py
+++ engine.py
@@ -1,13 +1,14 @@
 from math import log, exp
 
+
 class Value:
 
-    def __init__(self, data, _children = ()):
+    def __init__(self, data, _children=()):
         self.data = data
         self._prev = set(_children)
 
         self.grad = 0
-        self._backward = lambda : None
+        self._backward = lambda: None
 
     def __repr__(self):
         return f"data = {self.data}"
@@ -16,16 +17,20 @@ class Value:
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other))
+
         def _backward():
             self.grad += out.grad
             other.grad += out.grad
+
         out._backward = _backward
         return out
 
     def __neg__(self):
         out = Value(-self.data, (self,))
+
         def _backward():
             self.grad += -out.grad
+
         out._backward = _backward
         return out
 
@@ -35,18 +40,22 @@ class Value:
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other))
+
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
+
         out._backward = _backward
         return out
 
     def __pow__(self, other):
         other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data**other.data, (self, other))
+        out = Value(self.data ** other.data, (self, other))
+
         def _backward():
             self.grad += other.data * self.data ** (other.data - 1) * out.grad
             other.grad += self.data ** other.data * log(other.data) * out.grad
+
         out._backward = _backward
         return out
 
@@ -55,30 +64,44 @@ class Value:
 
     def exp(self):
         out = Value(exp(self.data), (self,))
+
         def _backward():
             self.grad += out * out.grad
+
         out._backward = _backward
         return out
 
     def log(self):
         out = Value(log(self.data), (self,))
+
         def _backward():
             self.grad += (self.data ** -1) * out.grad
+
         out._backward = _backward
+        return out
+
+    def tanh(self):
+        x = (self * 2).exp()
+        out = (x - 1) / (x + 1)
         return out
 
     def __rmul__(self, other):
         return self * other
 
+    def __radd__(self, other):
+        return self + other
+
     # backward method
     def backward(self):
         nodes = []  # Will contain each node in topological order
         visited = set()
+
         def build(v):
             nodes.append(v)
             if v not in visited:
                 visited.add(v)
                 for u in v._prev: build(u)
+
         build(self)
         for v in nodes:
             v._grad = 0.0
--- neural_net.py
+++ neural_net.py
@@ -1,15 +1,21 @@
 import random
 from engine import Value
 
+
 class Neuron:
     def __init__(self, nin):
-        #The bias and weights are randomly assigned a value between -1 and 1 when created
+        # The bias and weights are randomly assigned a value between -1 and 1 when created
         self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
         self.b = Value(random.uniform(-1, 1))
 
     def __call__(self, x):
-        # = sum( w * x) + b
-        return sum( (wt * x for wt, x in zip(self.w, x)), self.b)
+        # = tanh( sum( w * x) + b)
+        linear_result = sum((wt * x for wt, x in zip(self.w, x)), self.b)
+        return linear_result.tanh()
+
+    def parameters(self):
+        return self.w + [self.b]
+
 
 class Layer:
     def __init__(self, nin, nout):
@@ -17,18 +23,21 @@ class Layer:
 
     def __call__(self, x):
         out = [n(x) for n in self.neurons]
-        return out[0] if len(out) == 1  else out
+        return out[0] if len(out) == 1 else out
+
+    def parameters(self):
+        return [p for n in self.neurons for p in n.parameters()]
+
 
 class MLP:
-    def __init__(self, nin, nouts): # nouts is a list containing the no. of outputs for each layer
+    def __init__(self, nin, nouts):  # nouts is a list containing the no. of outputs for each layer
         sizes = [nin] + nouts
-        self.layers = [ Layer(sizes[i], sizes[i+1]) for i in range(len(sizes))]
+        self.layers = [Layer(sizes[i], sizes[i + 1]) for i in range(len(nouts))]
 
     def __call__(self, x):
         for layer in self.layers:
-            x = layer(x) # the input of the next layer is the ouput of the current layer
+            x = layer(x)  # the input of the next layer is the ouput of the current layer
         return x
 
-
-
-
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
